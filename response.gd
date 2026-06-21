7fba97a497b89ed77dc47b928c652fbe481db8d3
jamie <radioaverageash@proton.me>
2026-06-21 04:06:51 +0100

    boot ascii+ text wrapping

---
+++ response.gd
@@ -0,0 +1,28 @@
+extends Label
+
+var full_text = ""
+var cur_chars = 0
+var timer = 0
+var chars_per_second = 64
+var timer_paused = false
+
+
+func _ready() -> void:
+	full_text = text
+	text = ""
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	if cur_chars >= full_text.length():
+		return
+
+	if !timer_paused:
+		timer += delta
+		
+	var interval: float = 1.0 / chars_per_second
+
+	while timer >= interval and cur_chars < full_text.length():
+		timer -= interval
+		cur_chars += 1
+		text = full_text.substr(0, cur_chars)
--- scripts/console.gd
+++ scripts/console.gd
@@ -8,20 +8,34 @@ const InputResponse = preload("res://input_response.tscn")
 @onready var scrollbar = scroll.get_v_scroll_bar()
 
 @onready var command_processor = $CommandProcessor
+
+const Response = preload("res://response.tscn")
 var max_scroll_length := 0
 
 func _ready() -> void:
-	scrollbar.changed.connect(_handle_scrollbar_changed)
+	scrollbar.changed.connect(_handle_scrollbar_changed) #signal
 	max_scroll_length = scrollbar.max_value
+	
+	var boot_message = Response.instantiate()
+	var boot_ascii = FileAccess.open("res://koala.txt", FileAccess.READ)
+	boot_message.text = boot_ascii.get_as_text()
+	boot_ascii.close()
+	
+	boot_message.chars_per_second = 128
+
+	add_response(boot_message)
+
 
 func _on_input_text_submitted(new_text: String) -> void:
 	if(new_text.is_empty()): return
 	var input_response = InputResponse.instantiate()
 	var response = command_processor.process_command(new_text)
 	input_response.set_text(new_text, response)
-	history_rows.add_child(input_response)
-	
-	#scroll.scroll_vertical = scrollbar.max_value
+	add_response(input_response)
+
+
+func add_response(response: Control):
+	history_rows.add_child(response)
 
 
 func _handle_scrollbar_changed():
